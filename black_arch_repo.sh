bdda23ee70477f89ed089a0425ba4acc7b89acdd
Eric Valdez Jr <128732768+pyx9999@users.noreply.github.com>
2023-03-29 13:24:11 -0700

    Update black_arch_repo.sh

--- black_arch_repo.sh
+++ black_arch_repo.sh
@@ -6,9 +6,32 @@ curl -O https://blackarch.org/strap.sh
 
 echo 5ea40d49ecd14c2e024deecf90605426db97ea0c strap.sh | sha1sum -c
 
+echo $(whoami) "did the sha pass...?"
 
-# Set execute bit
-chmod +x strap.sh
+function confirm() {
+    while true; do
+        read -p "Do you want to proceed? (YES/NO/CANCEL) " yn
+        case $yn in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            [Cc]* ) exit;;
+            * ) echo "Please answer YES, NO, or CANCEL.";;
+        esac
+    done
+}
+ 
+# Example usage of the confirm function
+if confirm; then
+    echo "User chose YES. Executing the operation..."
+    chmod +x strap.sh
+else
+    echo "User chose NO. Aborting the operation..."
+    exit
+fi
+
+echo "now lets install the black arch repo..."
+
+
 
 # Run strap.sh
 ./strap.sh
